0237fb3e4c96e895e499635ba86479d74dcbab94
poyangpan <49646607+poyangpan@users.noreply.github.com>
2019-06-23 11:12:33 +0800

    2019_0622 update

--- carousel.js
+++ carousel.js
@@ -1,20 +1,24 @@
 window.onload = function () {
-    var dir = -1;
-    var interval = 3500;
-    var duration = 100;
+    var interval = 6000;
+    //var duration = 100;
     var timer;
 
-    $("#travel_slide img:nth-child(2)").addClass("hidden");
-    $("#travel_slide img:nth-child(3)").addClass("hidden");
+    // $("#travel_slide img:nth-child(2)").addClass("hidden");
+    // $("#travel_slide img:nth-child(3)").addClass("hidden");
 
     // 開始計時 
     // 每隔3000毫秒(interval)執行一次slideTimer
     timer = setInterval(slideTimer, interval);
-    slideTimer函數
+    //slideTimer函數
 
     function slideTimer() {
         $("#travel_slide img:nth-child(1)").addClass("hidden");
+        $("#sing_slide img:nth-child(1)").addClass("hidden");
+
         $("#travel_slide img:nth-child(3)").removeClass("hidden");
+        $("#sing_slide img:nth-child(3)").removeClass("hidden");
+
         $("#travel_slide").prepend($("#travel_slide img:nth-child(3)"));
+        $("#sing_slide").prepend($("#sing_slide img:nth-child(3)"));
     };
 }
