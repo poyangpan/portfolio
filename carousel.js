d56a029581fe740b4a4998ccf506552f1c234e33
poyangpan <49646607+poyangpan@users.noreply.github.com>
2019-09-26 22:47:42 +0800

    0926 update

--- carousel.js
+++ carousel.js
@@ -1,24 +1,43 @@
 window.onload = function () {
-    var interval = 6000;
-    //var duration = 100;
+    var dir = -1;
+    var interval = 3500;
+    var duration = 100;
     var timer;
 
-    // $("#travel_slide img:nth-child(2)").addClass("hidden");
-    // $("#travel_slide img:nth-child(3)").addClass("hidden");
+    $("#travel_slide img:nth-child(2)").addClass("hidden");
+    $("#travel_slide img:nth-child(3)").addClass("hidden");
 
     // 開始計時 
     // 每隔3000毫秒(interval)執行一次slideTimer
     timer = setInterval(slideTimer, interval);
-    //slideTimer函數
+    slideTimer函數
 
     function slideTimer() {
         $("#travel_slide img:nth-child(1)").addClass("hidden");
-        $("#sing_slide img:nth-child(1)").addClass("hidden");
-
         $("#travel_slide img:nth-child(3)").removeClass("hidden");
-        $("#sing_slide img:nth-child(3)").removeClass("hidden");
-
         $("#travel_slide").prepend($("#travel_slide img:nth-child(3)"));
-        $("#sing_slide").prepend($("#sing_slide img:nth-child(3)"));
     };
 }
+
+// function animate(offset) {
+//     // 我們可以透過直接變動html上的style變更圖片位置
+//     //獲取的是style.left，是相對左邊獲取距離，所以第一張圖後style.left都為負值，
+//     //且style.left獲取的是字符串，需要用parseInt()取整轉化為數字。
+//     var newLeft = parseInt(list.style.top) + offset;
+//     list.style.top = newLeft + '%';
+//     if (newLeft < -400) {
+//         list.style.top = 0 + '%';
+//     }
+//     if (newLeft > 0) {
+//         list.style.top = -400 + '%';
+//     }
+// }
+
+// prev.onclick = function () {
+//     // 按下往前的鈕 變數offset+600回去
+//     animate(100);
+// };
+// next.onclick = function () {
+//     // 按下往後的鈕 變數offset-600
+//     animate(-100);
+// };
